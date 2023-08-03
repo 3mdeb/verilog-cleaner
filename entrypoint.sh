5aeb707fd0f86510f2178f9e7dc1c058b564f44f
Michał Żygowski <michal.zygowski@3mdeb.com>
2023-08-03 10:55:30 +0200

    entrypoint.sh: use static executables for Linux x86_64
    Signed-off-by: Michał Żygowski <michal.zygowski@3mdeb.com>

--- entrypoint.sh
+++ entrypoint.sh
@@ -28,7 +28,7 @@ command -v verilator \
 ###### verible ######
 command -v verible-verilog-lint \
     || curl -s https://api.github.com/repos/chipsalliance/verible/releases/latest \
-    | grep -oP '\s*"browser_download_url":\s*"\K.*'$(lsb_release -r | cut -f 2)'.*(?=")' \
+    | grep -oP '\s*"browser_download_url":\s*"\K.*'linux-static-x86_64'.*(?=")' \
     | xargs curl -L | sudo tar xzf - -h --strip-components 1 -C /usr
 
 reviewdog -reporter=${INPUT_REPORTER-local} -diff='git diff' -runners=${INPUT_RUNNERS} -conf=${ACTION_PATH-.}/.reviewdog.yml -tee
